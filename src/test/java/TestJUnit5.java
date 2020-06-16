a6b33691cbf5cd56798ca0382c837e29c4b5e5d2
guzhenhua <7070@qq.com>
2020-06-16 18:00:09 +0800

    'fix'

---
+++ src/test/java/TestJUnit5.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestJUnit5 {
+    @Test
+    void test(){
+        assertTrue(false);
+    }
+}
