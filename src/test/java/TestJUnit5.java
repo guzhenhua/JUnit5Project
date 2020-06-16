c02602e46080940074d0f9f74603aaf0f603f378
guzhenhua <7070@qq.com>
2020-06-16 19:36:53 +0800

    'assertAll'

--- src/test/java/TestJUnit5.java
+++ src/test/java/TestJUnit5.java
@@ -1,11 +1,15 @@
 import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TestJUnit5 {
     @Test
-    void test(){
-        assertTrue(false);
+    void test() {
+        assertAll("test",
+                () -> assertEquals(1, 2),
+                () -> assertEquals(1,1),
+                () -> assertEquals(1,3)
+        );
     }
 }
